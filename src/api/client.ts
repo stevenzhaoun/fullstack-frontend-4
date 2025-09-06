956f4772c73655292683d3495c42773043d39a1e
Steven Zhao <stevenzhao0819@gmail.com>
2025-09-05 20:15:31 -0700

    update client address

--- src/api/client.ts
+++ src/api/client.ts
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const client = axios.create({
-    baseURL: 'http://localhost:8888',
+    baseURL: 'https://fullstack-backend-4-5utc.onrender.com',
 });
 
 export default client
